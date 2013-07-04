f4c2a90197496e8292f156a1a85a7b03afcb4cd7
Yuva <yuva@codemancers.com>
2013-07-04 15:24:34 +0530

    add barebone template

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RapidfireDemo::Application.config.secret_token = '2acd73315354a237863e222c48042ee28dcbff6540a03e0b159e2db85a0954b96b47cf65e882d72bded43f1d2799eefd12598b7d61481d3dd435229ccd94ec70'
