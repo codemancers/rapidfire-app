fc2a7ff6c3e042f65bf7dbe92ec2073d85a19f60
Yuva <yuva@codemancers.com>
2015-07-16 10:56:56 +0530

    enqueue survey attempted job if any user takes a survey

--- app/assets/javascripts/surveys.js.coffee
+++ app/assets/javascripts/surveys.js.coffee
@@ -2,4 +2,4 @@
 
 RapidfireDemo.surveys = RapidfireDemo.cable.subscriptions.create "SurveysChannel",
   received: (data)->
-    alert(data)
+    alert "survey " + data.survey_id + " has been attempted just now"
--- app/channels/base_connection.rb
+++ app/channels/base_connection.rb
@@ -1,17 +1,2 @@
 class BaseConnection < ActionCable::Connection::Base
-  identified_by :current_user
-
-  def connect
-    self.current_user = User.first
-  end
-
-  protected
-
-  def find_verified_user
-    if current_user = User.find_by(id: cookies.signed[:user_id])
-      current_user
-    else
-      reject_unauthorized_connection
-    end
-  end
 end
