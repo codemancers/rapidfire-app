e8819358a6034ecb67d00c773d03491754daa294
Harshwardhan Singh Rathore <harsh.rathore14@gmail.com>
2016-09-29 18:59:03 +0530

    upgrade to rails 5.0.0.1 and using ruby 2.3.1

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
-ruby '2.2.2'
+ruby '2.3.1'
 
-gem 'rails', '~> 4.2.3'
+gem 'rails', '~> 5.0', '>= 5.0.0.1'
 gem 'pg'
 
 gem 'sass-rails',   '~> 5.0'
@@ -21,7 +21,6 @@ gem 'bcrypt', '~> 3.1.7'
 
 group :development do
   gem 'pry-remote', '~> 0.1.7'
-  gem 'quiet_assets'
   gem 'spring'
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -18,57 +17,53 @@ ActiveRecord::Schema.define(version: 20150627051030) do
 
   create_table "rapidfire_answer_groups", force: :cascade do |t|
     t.integer  "question_group_id"
+    t.string   "user_type"
     t.integer  "user_id"
-    t.string   "user_type",         limit: 255
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["question_group_id"], name: "index_rapidfire_answer_groups_on_question_group_id", using: :btree
+    t.index ["user_id", "user_type"], name: "index_rapidfire_answer_groups_on_user_id_and_user_type", using: :btree
   end
-
-  add_index "rapidfire_answer_groups", ["question_group_id"], name: "index_rapidfire_answer_groups_on_question_group_id", using: :btree
-  add_index "rapidfire_answer_groups", ["user_id", "user_type"], name: "index_rapidfire_answer_groups_on_user_id_and_user_type", using: :btree
 
   create_table "rapidfire_answers", force: :cascade do |t|
     t.integer  "answer_group_id"
     t.integer  "question_id"
     t.text     "answer_text"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["answer_group_id"], name: "index_rapidfire_answers_on_answer_group_id", using: :btree
+    t.index ["question_id"], name: "index_rapidfire_answers_on_question_id", using: :btree
   end
 
-  add_index "rapidfire_answers", ["answer_group_id"], name: "index_rapidfire_answers_on_answer_group_id", using: :btree
-  add_index "rapidfire_answers", ["question_id"], name: "index_rapidfire_answers_on_question_id", using: :btree
-
   create_table "rapidfire_question_groups", force: :cascade do |t|
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "rapidfire_questions", force: :cascade do |t|
     t.integer  "question_group_id"
-    t.string   "type",              limit: 255
-    t.string   "question_text",     limit: 255
+    t.string   "type"
+    t.string   "question_text"
     t.integer  "position"
     t.text     "answer_options"
     t.text     "validation_rules"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["question_group_id"], name: "index_rapidfire_questions_on_question_group_id", using: :btree
   end
-
-  add_index "rapidfire_questions", ["question_group_id"], name: "index_rapidfire_questions_on_question_group_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",              limit: 255, default: "", null: false
-    t.string   "password_digest",    limit: 255, default: "", null: false
-    t.integer  "sign_in_count",                  default: 0
+    t.string   "email",              default: "", null: false
+    t.string   "password_digest",    default: "", null: false
+    t.integer  "sign_in_count",      default: 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip", limit: 255
-    t.string   "last_sign_in_ip",    limit: 255
-    t.datetime "created_at",                                  null: false
-    t.datetime "updated_at",                                  null: false
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
