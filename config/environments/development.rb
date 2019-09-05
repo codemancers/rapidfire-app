c9a8e7fbb196554ba3463190f2104eec9518eece
Shubham Shukla <snkshukla@gmail.com>
2019-09-05 16:52:54 +0530

    Force ssl in production & change develpment host to rapidfire.test

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,7 +9,7 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
 
-  config.hosts << "rapidfire-client.test"
+  config.hosts << "rapidfire.test"
 
   # Show full error reports.
   config.consider_all_requests_local = true
