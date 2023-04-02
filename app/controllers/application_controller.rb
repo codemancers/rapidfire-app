36461013ec303fc1362aa52318385ffbebea2e2f
Yuva <yuva@codemancers.com>
2023-04-02 20:06:35 +0530

    Get homepage working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def can_administer?
-      current_user.try(:admin?)
+    current_user.try(:admin?)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,25 +2,25 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_01_102811) do
-
+ActiveRecord::Schema[7.0].define(version: 2019_09_01_102811) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "postgis"
 
   create_table "rapidfire_answers", force: :cascade do |t|
     t.bigint "attempt_id"
     t.bigint "question_id"
     t.text "answer_text"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["attempt_id"], name: "index_rapidfire_answers_on_attempt_id"
     t.index ["question_id"], name: "index_rapidfire_answers_on_question_id"
   end
@@ -29,11 +29,11 @@ ActiveRecord::Schema.define(version: 2019_09_01_102811) do
     t.bigint "survey_id"
     t.string "user_type"
     t.bigint "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["survey_id"], name: "index_rapidfire_attempts_on_survey_id"
     t.index ["user_id", "user_type"], name: "index_rapidfire_attempts_on_user_id_and_user_type"
-    t.index ["user_type", "user_id"], name: "index_rapidfire_attempts_on_user_type_and_user_id"
+    t.index ["user_type", "user_id"], name: "index_rapidfire_attempts_on_user"
   end
 
   create_table "rapidfire_questions", force: :cascade do |t|
@@ -45,27 +45,35 @@ ActiveRecord::Schema.define(version: 2019_09_01_102811) do
     t.integer "position"
     t.text "answer_options"
     t.text "validation_rules"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["survey_id"], name: "index_rapidfire_questions_on_survey_id"
   end
 
   create_table "rapidfire_surveys", force: :cascade do |t|
     t.string "name"
     t.text "introduction"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.text "after_survey_content"
+  end
+
+  create_table "spatial_ref_sys", primary_key: "srid", id: :integer, default: nil, force: :cascade do |t|
+    t.string "auth_name", limit: 256
+    t.integer "auth_srid"
+    t.string "srtext", limit: 2048
+    t.string "proj4text", limit: 2048
+    t.check_constraint "srid > 0 AND srid <= 998999", name: "spatial_ref_sys_srid_check"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.datetime "reset_password_sent_at", precision: nil
+    t.datetime "remember_created_at", precision: nil
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
